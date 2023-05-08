dcc46175679994bb98dee4d328e1b7f611d26a53
savyviip <savy.viip@gmail.com>
2023-05-08 04:22:45 -0300

    Proyecto individual

--- PI-Dogs-main/client/src/Components/Detail/Detail.jsx
+++ PI-Dogs-main/client/src/Components/Detail/Detail.jsx
@@ -4,28 +4,36 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function Detail() {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const { dogs } = useSelector((state) => state);
-    const dog = dogs.filter((element) =>
-        element.id === Number(id)
-    )
-    console.log(dog)
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { dogs } = useSelector((state) => state);
+  const dog = dogs.filter((element) => element.id === Number(id));
+  const { age, height, image, name, temperaments, weight } = dog[0];
 
-    const { age, height, image, name, temperaments, weight, } = dog[0];
-
-    return (
-        <>
-            {/* <div className="btnClose" onClick={() => navigate(-1)}> Volver  </div> // navigate vuelve 1- menos */}
-            <div className={style.conteiner}>
-                <img src={image} alt='' />
-                <h1>Codigo: {id}</h1>
-                <h1>Name: {name}</h1>
-                <h1>Age: {age}</h1>
-                <h1>Altura {height}</h1>
-                <h1>Peso {weight}</h1>
-                <h1>Temperamentos {temperaments.join(", ")}</h1>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="btnClose" onClick={() => navigate(-1)}>
+        Volver
+      </div>
+      <div className={style.cardContainer}>
+        <div className={style.card}>
+          <div className={style.cardImage}>
+            <img src={image} alt="" />
+          </div>
+          <div className={style.cardContent}>
+            <h1 className={style.cardTitle}>Codigo: {id}</h1>
+            <h2 className={style.cardSubtitle}>Name: {name}</h2>
+            <p className={style.cardText}>Age: {age}</p>
+            <p className={style.cardText}>Altura {height}</p>
+            <p className={style.cardText}>Peso {weight}</p>
+            <p className={style.cardText}>
+              Temperamentos {temperaments.join(", ")}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
+{/* <div className="btnClose" onClick={() => navigate(-1)}> Volver  </div> // navigate vuelve 1- menos */ }
